38dd9bbd39da826131dd18084916d6e7d41b422b
Sean Whittemore <whittemore_sean@students.columbusstate.edu>
2025-01-19 19:27:56 -0500

    Add First.java program printing

---
+++ First.java
@@ -0,0 +1,11 @@
+// Name: Sean Whittemore
+// This is a simple Java program to print some details
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+        System.out.println("Job title: Cybersecurity Analyst");
+        System.out.println("Why: Passionate about cybersecurity and interested in the field");
+        System.out.println("Median salary: $120,360");
+    }
+}
